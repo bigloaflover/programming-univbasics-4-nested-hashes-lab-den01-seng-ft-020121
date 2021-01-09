aa59a1724a0a6b9b3c8ccc98fe09b96e7e061b5c
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-09 06:46:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,5 +1,6 @@
 
 def hopper
+  programmer_hash[:grace_hopper][:known_for]
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -20,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:grace_hopper][:known_for]
+	
 	programmer_hash = 
  		{
       :grace_hopper => {
